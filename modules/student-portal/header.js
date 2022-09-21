35e521d5c2e1d6149c1ab2cc97ee0fcea5475b98
ice949 <justineimasiku21@gmail.com>
2022-09-21 17:57:14 +0200

    remove drop-down enu on others page

--- modules/student-portal/header.js
+++ modules/student-portal/header.js
@@ -7,18 +7,7 @@ const headerPageData2 = () => {
       <li class="close" id="close"><i class="fa-solid fa-xmark"></i></li>
       <li class="results" id="results">Results</li>
       <li class="library" id="library">Library</li>
-      <li class="other" id="other">
-        <div class="dropdown">
-          <p class="dropbtn">Other</p>
-          <div class="dropdown-content">
-            <a href="#" id="attendance">Attendance</a>
-            <a href="#" id="calender">Calender</a>
-            <a href="#" id="t-table">Time-Table</a>
-            <a href="#" id="bills">Bills</a>
-            <a href="#" id="clubs">clubs</a>
-          </div>
-        </div>
-      </li>
+      <li class="other" id="other">Other</li>
     </ul>
            <div class="humburger-container" id="menu2">
     <i class="fa-sharp fa-solid fa-bars"></i>
